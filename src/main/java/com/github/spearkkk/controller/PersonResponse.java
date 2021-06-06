37277ccba60f0965f82555e7a03b7cc21df656de
spearkkk <spearkkp@gmail.com>
2021-06-06 22:21:15 +0900

    Change to use builder pattern in mapper

--- src/main/java/com/github/spearkkk/controller/PersonResponse.java
+++ src/main/java/com/github/spearkkk/controller/PersonResponse.java
@@ -1,9 +1,14 @@
 package com.github.spearkkk.controller;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Value;
 
+@Value
 @Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class PersonResponse {
-    private Long id;
-    private String name;
+    Long id;
+    String name;
 }
--- src/main/java/com/github/spearkkk/domain/Person.java
+++ src/main/java/com/github/spearkkk/domain/Person.java
@@ -3,6 +3,7 @@ package com.github.spearkkk.domain;
 import lombok.*;
 
 @Value
+@Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Person {
     Long id;
