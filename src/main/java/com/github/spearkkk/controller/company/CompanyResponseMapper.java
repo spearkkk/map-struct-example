3c391d153504f50b45b8e9193cac0ba2962dae86
spearkkk <spearkkp@gmail.com>
2021-07-31 14:53:35 +0900

    Fix bug

--- src/main/java/com/github/spearkkk/controller/company/CompanyResponseMapper.java
+++ src/main/java/com/github/spearkkk/controller/company/CompanyResponseMapper.java
@@ -1,5 +1,6 @@
 package com.github.spearkkk.controller.company;
 
+import com.github.spearkkk.controller.util.mapper.BaseDatetimeToString;
 import com.github.spearkkk.domain.company.Company;
 import org.mapstruct.Mapper;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CompanyResponseMapper {
+  @BaseDatetimeToString
   CompanyResponse map(Company entity);
   List<CompanyResponse> map(List<Company> entities);
 }
